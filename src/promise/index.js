c1602ebdfe56c8f2477de0ab1138a03901b72027
Codekacode <karem.quiroz.m@gmail.com>
2022-01-15 00:47:35 -0500

    Promise2

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,7 +1,7 @@
 //ES6
 const somethingWillHappen = () => {
   return new Promise((resolve, reject) => {
-    if(false) {
+    if(true) {
       resolve('Hey is correct!')
     } else {
       reject('Upss!')
@@ -13,3 +13,30 @@ const somethingWillHappen = () => {
 somethingWillHappen()
 .then(response => console.log(response))
 .catch(err => console.error(err))
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if(true) {
+      setTimeout(() => {
+        resolve('True');
+      }, 2000)
+    } else {
+      const error = new Error('upsss!!!') //nos da un error mas descria para debugear mejor
+      reject(error);
+    }
+  });
+};
+
+somethingWillHappen2()
+.then(response => console.log(response))
+.catch(err => console.error(err))
+
+//correr promesas encadenadas Promise.all que retoranara una arreglo
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+.then(response =>{
+  console.log('Array de resultados', response)
+})
+.catch(err => {
+  console.error(err);
+})
